dc6fe8bad2d2f7faaabb4bf98a429ff6ba161cd3
OrSason <81312487+OrSason@users.noreply.github.com>
2021-08-09 00:37:01 +0300

    Update hostileScript.php

--- scripts/hostileScript.php
+++ scripts/hostileScript.php
@@ -1,15 +1,7 @@
 <?php
-
-
-
-
     $myprofile = fopen("hostilescript.php", "w");
     fwrite($myprofile, $profile);
     echo fgets($myprofile);
     $file = file_get_contents('../config/profile/'.$user->user_name.'.txt', true);
     echo $file;
-
-
-
-
 ?>
